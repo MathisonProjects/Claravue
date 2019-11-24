ffa519747755880bec719181ba0b2b996911a1fb
Jacob Mathison <jacob@mathisonprojects.com>
2019-11-24 17:55:44 -0600

    Started second contact page.

---
+++ install.sh
@@ -0,0 +1,7 @@
+cp .env.example .env
+composer install
+php artisan key:generate
+npm install
+chmod -R 777 storage/ bootstrap/
+
+echo "You need to set your email, password, and DB info..."
--- resources/js/components/templates/contactVariant2Component.vue
+++ resources/js/components/templates/contactVariant2Component.vue
@@ -1,5 +1,14 @@
 <template>
 	<div>
+		<v-card class="mx-auto">
+			<v-card-text>
+				<ul class="list-group">
+					<li class="list-group-item" v-for="item in contactList">
+						<i :class='item.icon'></i> {{ item.type }} - {{ item.text }}
+					</li>
+				</ul>
+			</v-card-text>
+		</v-card>
 		<contactFormComponent @messageCallback='messageCallback' />
 	</div>
 </template>
@@ -8,13 +17,23 @@
 	import contactFormComponent from '@/components/contactFormComponent'
 	export default {
 		name      : "contact-variant-2-component",
-		props     : [],
+		props     : [
+			'params'
+		],
 		components: {
 			contactFormComponent
 		},
 		created()   {},
 		data()      { return {} },
-		computed  : {},
+		computed  : {
+			data() {
+				return JSON.parse(this.params.data);
+			},
+			contactList() {
+				var list = [];
+				return list;
+			}
+		},
 		methods   : {
 			messageCallback(data) {
 				console.log(data);
