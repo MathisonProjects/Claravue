3881f0021a0d766fbd4d70f6d8d49cf69730f5fe
Jacob Mathison <jacob@mathisonprojects.com>
2020-01-12 22:36:34 -0600

    Travis test...

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,15 +7,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function testBasicTest()
     {
-        $response = $this->get('/');
-
         $response->assertStatus(200);
     }
 }
