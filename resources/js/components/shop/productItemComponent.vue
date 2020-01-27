3edcf36baf8fc7b569bebd3e0e32fc2ba805d276
Jacob Mathison <jacob@mathisonprojects.com>
2020-01-26 19:24:10 -0600

    Claravue image fix

--- resources/js/components/shop/productItemComponent.vue
+++ resources/js/components/shop/productItemComponent.vue
@@ -4,7 +4,8 @@
 			<h5>{{ product.name }}</h5>
 			<img :src='imageUrl' class='w-100' />
 			<p>{{ product.short_description }}</p>
-			<button type='button' class='btn btn-primary btn-sm' @click='addProduct'><i class='fas fa-shopping-cart'></i> ${{ $Helper.formatHelper.formatCurrency(product.amount) }}</button>
+			<button type='button' class='btn btn-primary btn-sm' @click='addProduct' v-if='!added'><i class='fas fa-shopping-cart'></i> ${{ $Helper.formatHelper.formatCurrency(product.amount) }}</button>
+			<button type='button' class='btn btn-success btn-sm' @click='addProduct' v-if='added'><i class='fas fa-shopping-cart'></i> Put in Cart</button>
 			<button type='button' class='btn btn-primary btn-sm' @click='viewProduct'><i class='fas fa-search'></i> View</button>
 		</v-card-text>
 	</v-card>
@@ -18,7 +19,11 @@
 		],
 		components: {},
 		created()   {},
-		data()      { return {} },
+		data()      {
+			return {
+				added: false
+			}
+		},
 		computed  : {
 			imageUrl() {
 				return '/upload/' + this.product.meta.images[0];
@@ -28,12 +33,19 @@
 			addProduct() {
 				this.$store.dispatch('cartStore/addToCart', this.product);
 				this.$Helper.alertHelper.addedToCart();
+				this.added = true;
 			},
 			viewProduct() {
 				this.$router.push('/shop/' + this.product.id);
 			}
 		},
-		watch     : {}
+		watch     : {
+			added(newVal) {
+				if (newVal == true) {
+					setTimeout( () => { this.added = false }, 3000)
+				}
+			}
+		}
 	};
 </script>
 
