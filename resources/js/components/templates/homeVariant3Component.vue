08bb26121f123225aa60f6cc03379f92014b5f5c
Jacob Mathison <jacob@mathisonprojects.com>
2020-02-01 08:09:10 -0600

    Cleaned up homeVariant3Component

--- resources/js/components/templates/homeVariant3Component.vue
+++ resources/js/components/templates/homeVariant3Component.vue
@@ -1,7 +1,11 @@
 <template>
 	<div>
 		<div class='row'>
-			<div class='col-xs-12 col-sm-6 col-md-6 col-md-4' v-html='content'></div>
+			<div class='col-xs-12 col-sm-6 col-md-6 col-md-4'>
+				<v-card class="mx-auto">
+					<v-card-text v-html='content'></v-card-text>
+				</v-card>
+			</div>
 		</div>
 	</div>
 </template>
